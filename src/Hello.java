08e0f1dcc84452c27fc491577bf679fee4f3d701
VanLien1503 <hoanglien89nd@gmail.com>
2020-04-27 11:24:29 +0700

    BT_Hiển Thị Lời Chào

--- src/Hello.java
+++ src/Hello.java
@@ -1,2 +1,12 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Hello {
+    public static void main(String[] args) {
+        Scanner name=new Scanner(System.in);
+        System.out.println("enter Your Name");
+        String yourName=name.nextLine();
+        System.out.println("Hello "+ yourName);
+
+    }
 }
